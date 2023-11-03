4583807edd0523fadf29ddaa9c5f5bce7f722bd1
kronos <dua_bhavya@outlook.com>
2023-11-03 16:36:25 -0400

    Add create action for Games resource

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,15 @@ class GamesController < ApplicationController
     def index
         render json: Game.all, include: ['reviews', 'reviews.user']
     end
+
+    def create
+        game = Game.create!(game_params)
+        render json: game
+    end
+
+    private
+
+    def game_params
+        params.permit([:title, :publisher, :game_img, :release_year])
+    end
 end
