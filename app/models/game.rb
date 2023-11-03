107f1e39c8a12b6e8d29b5d8ae0cf421c82f2cbf
kronos <dua_bhavya@outlook.com>
2023-11-03 15:54:52 -0400

    Add validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,14 @@
 class Game < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    validates :title, presence: true, uniqueness: true
+    validates :publisher, presence: true
+    validate :valid_release_year
+
+    def valid_release_year
+        if release_year && release_year > Date.current.year
+            errors.add(:release_year, "Game has not released yet")
+        end
+    end
 end
