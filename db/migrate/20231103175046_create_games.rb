0d51d834ec07dca12af6c6dab1ec5d34e55eb74d
kronos <dua_bhavya@outlook.com>
2023-11-03 14:04:03 -0400

    Migrate games table

--- db/migrate/20231103175046_create_games.rb
+++ db/migrate/20231103175046_create_games.rb
@@ -1,6 +1,10 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
+      t.string :title
+      t.string :publisher
+      t.string :game_img
+      t.integer :release_year
 
       t.timestamps
     end
