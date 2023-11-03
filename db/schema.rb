7a7745a786177847dd5d49af8dcb045296256bfe
kronos <dua_bhavya@outlook.com>
2023-11-03 15:21:53 -0400

    Migrate users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_03_180438) do
+ActiveRecord::Schema.define(version: 2023_11_03_181527) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2023_11_03_180438) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["game_id"], name: "index_reviews_on_game_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
