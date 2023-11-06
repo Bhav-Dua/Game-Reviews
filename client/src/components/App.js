3e06075a0f1930ed9e643cc0593780fddd8da98a
kronos <dua_bhavya@outlook.com>
2023-11-06 17:04:47 -0500

    Implement auto-login and context functionality for user

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const UserContext = React.createContext();
+
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me")
+      .then(r => {
+        if (r.ok) {
+          r.json()
+            .then(setUser)
+        }
+      })
+  })
+
+
+
+
   return (
     <div className="App">
-
+      <UserContext.Provider value={user}>
+      </UserContext.Provider>
     </div>
   );
 }
