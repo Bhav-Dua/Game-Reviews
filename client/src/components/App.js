5e986247ab62da6b00d24b7d44df54df88fcb69f
kronos <dua_bhavya@outlook.com>
2023-11-06 19:15:47 -0500

    Create logout function and user conditionally rendered html

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 import './App.css';
 import NavBar from './NavBar';
 
-const UserContext = React.createContext();
+export const UserContext = createContext();
 
 function App() {
 
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <UserContext.Provider value={user}>
-        <NavBar />
+        <NavBar setUser={setUser}/>
       </UserContext.Provider>
     </div>
   );
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom"
+import { UserContext } from "./App"
 
-function NavBar() {
+function NavBar({ setUser }) {
 
+    const user = useContext(UserContext);
+
+    function handleLogout() {
+        fetch("/logout", { method: "DELETE" })
+            .then(r => {
+                if (r.ok)
+                setUser(null);
+            })
+    }
 
     return (
         <div className="NavBar">
             <h1 className="Nav-logo">
                 <Link to="/">Games</Link>
             </h1>
+            {user ? (
+                <div className="User-action">
+                <p>Hello {user.username}</p>
+                <button onClick={handleLogout}>Logout</button>
+                </div>
+            ) : (
+                <Link className="User-action" to="/login">Login</Link>
+            )}
         </div>
     )
 
