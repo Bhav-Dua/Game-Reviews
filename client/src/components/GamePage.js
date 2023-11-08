ffb9c9e7cf67ef4f3e7facc183066bc34a422327
kronos <dua_bhavya@outlook.com>
2023-11-08 00:17:10 -0500

    Remove unneccessary imports

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Review from "./Review";
 
