572a5450233e6f62886e12704d8d5995c05ba485
kronos <dua_bhavya@outlook.com>
2023-11-08 00:18:37 -0500

    Format document

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -3,28 +3,22 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Review from "./Review";
 
 function GamePage({ games }) {
+  const { id } = useParams();
+  const gameToDisplay = games.find((game) => game.id == id);
 
-    const { id } = useParams();
-    const gameToDisplay = games.find((game) => game.id == id)
+  if (!gameToDisplay) return <div>Game not found</div>;
 
-    if (!gameToDisplay) return <div>Game not found</div>
+  const reviewsToDisplay = gameToDisplay.reviews.map((review, index) => (
+    <Review
+      key={index}
+      content={review.content}
+      rating={review.rating}
+      username={review.user.username}
+      userId={review.user.id}
+    />
+  ));
 
-    const reviewsToDisplay = gameToDisplay.reviews.map((review, index) => (
-        <Review
-            key={index}
-            content={review.content}
-            rating={review.rating}
-            username={review.user.username}
-            userId={review.user.id}
-        />
-    ))
-
-
-    return (
-        <div className="ui vertical menu">
-            {reviewsToDisplay}
-        </div>
-    )
+  return <div className="ui vertical menu">{reviewsToDisplay}</div>;
 }
 
 export default GamePage;
