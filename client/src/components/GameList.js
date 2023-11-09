68f1cbdcb7c394abddd60fb1a02645ecd9277973
kronos <dua_bhavya@outlook.com>
2023-11-08 20:02:00 -0500

    Add publisher for each gamecard

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { UserContext } from "./App";
 
 function GameList({ games }) {
   const history = useHistory();
+  const user = useContext(UserContext);
 
   function handleAddGame() {
     history.push("/addgame");
   }
 
+  function handleMyGames() {
+    history.push("/mygames");
+  }
+
   return (
     <div className="GameList">
-      <button
-        className="AddGame"
-        onClick={handleAddGame}
-        style={{ marginBottom: "3rem" }}
-      >
-        Add Game
-      </button>
+      {user ? (
+        <div className="ui buttons" style={{ marginBottom: "3rem" }}>
+          <button className="ui button" onClick={handleAddGame}>
+            Add Game
+          </button>
+          <button className="ui button" onClick={handleMyGames}>
+            My Games
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className="ui cards">
         {games.map((game) => (
           <div key={game.id} className="ui card">
@@ -27,6 +38,7 @@ function GameList({ games }) {
               <a className="header" href={`/games/${game.id}`}>
                 {game.title}
               </a>
+              <h5>Publisher: {game.publisher}</h5>
               {game.release_year ? (
                 <div className="description">
                   Released in {game.release_year}
